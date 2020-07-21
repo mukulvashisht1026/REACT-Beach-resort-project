cfb105067af3bff6814ba31c8130e4ccc08f130f
mukulvashisht1026 <mukulvashisht1026@gmail.com>
2020-07-21 21:18:32 +0530

    Updates

---
+++ precache-manifest.ce93235b17ce64f8548d79050e8fdbe6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c28f5656f05e970cb5a1f4769ad83fa8",
+    "url": "/REACT-Beach-resort-project/index.html"
+  },
+  {
+    "revision": "57872f4a77e9bc80fa40",
+    "url": "/REACT-Beach-resort-project/static/css/main.2c2aea84.chunk.css"
+  },
+  {
+    "revision": "0e90b2ac78ec7d106d2c",
+    "url": "/REACT-Beach-resort-project/static/js/2.e1fdb1f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/REACT-Beach-resort-project/static/js/2.e1fdb1f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57872f4a77e9bc80fa40",
+    "url": "/REACT-Beach-resort-project/static/js/main.0882c45a.chunk.js"
+  },
+  {
+    "revision": "97b0d347856b24948b1c",
+    "url": "/REACT-Beach-resort-project/static/js/runtime-main.1b54fc07.js"
+  },
+  {
+    "revision": "aebaec51b2f82a2993fda821ca04e228",
+    "url": "/REACT-Beach-resort-project/static/media/defaultBcg.aebaec51.jpeg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/REACT-Beach-resort-project/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "9f0cccc1e406a5aa9ef6ee36229762c3",
+    "url": "/REACT-Beach-resort-project/static/media/logo.9f0cccc1.svg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/REACT-Beach-resort-project/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/REACT-Beach-resort-project/static/media/room-2.052c7681.jpeg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/REACT-Beach-resort-project/precache-manifest.ce93235b17ce64f8548d79050e8fdbe6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/REACT-Beach-resort-project/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
